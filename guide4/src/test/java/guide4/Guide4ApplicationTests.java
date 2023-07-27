6669ff60517a22ba3e6eeb9bf32b68b304fc99fc
Jung-YouHwan <yhwjd99@gmail.com>
2023-07-27 14:27:06 +0900

    Guide 4 - Accessing Relational Data using JDBC with Spring

---
+++ guide4/src/test/java/guide4/Guide4ApplicationTests.java
@@ -0,0 +1,13 @@
+package guide4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Guide4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
