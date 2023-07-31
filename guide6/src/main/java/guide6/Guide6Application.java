e334e911d72c8bb4836f724adc6e6c663f40be8b
Jung-YouHwan <yhwjd99@gmail.com>
2023-07-31 18:42:14 +0900

    guide 6 - Authenticating a User with LDAP

---
+++ guide6/src/main/java/guide6/Guide6Application.java
@@ -0,0 +1,13 @@
+package guide6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Guide6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Guide6Application.class, args);
+    }
+
+}
---
+++ guide6/src/test/java/guide6/Guide6ApplicationTests.java
@@ -0,0 +1,13 @@
+package guide6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Guide6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
