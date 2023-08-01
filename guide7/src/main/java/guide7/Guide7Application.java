5a1ed610749586b171d24bda66f61c4ef2677bc9
Jung-YouHwan <yhwjd99@gmail.com>
2023-08-01 14:00:33 +0900

    Guide 7 - Messaging with Redis 예제 코드

---
+++ guide7/src/main/java/guide7/Guide7Application.java
@@ -0,0 +1,31 @@
+package guide7;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@SpringBootApplication
+public class Guide7Application {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(RedisConfig.class);
+
+	public static void main(String[] args) throws InterruptedException {
+
+		ApplicationContext ctx = SpringApplication.run(Guide7Application.class, args);
+
+		StringRedisTemplate template = ctx.getBean(StringRedisTemplate.class);
+		Receiver receiver = ctx.getBean(Receiver.class);
+
+		while (receiver.getCount() == 0) {
+			LOGGER.info("Sending message...");
+			template.convertAndSend("chat", "Hello from Redis!");
+			Thread.sleep(500L);
+		}
+
+		System.exit(0);
+	}
+
+}
